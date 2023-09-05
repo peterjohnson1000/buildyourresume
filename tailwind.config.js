b684841cb9269c3cacd56d66a0d9c7a14d6debee
peterjohnson1000 <peterj1298@gmail.com>
2023-09-05 18:07:53 +0530

    Closes #1

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,30 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "2xl": { max: "1535px" },
+        // => @media (max-width: 1535px) { ... }
+  
+        xl: { max: "1279px" },
+        // => @media (max-width: 1279px) { ... }
+  
+        xll: { max: "1200px" },
+        // => @media (max-width: 1200px) { ... }
+  
+        lg: { max: "1023px" },
+        // => @media (max-width: 1023px) { ... }
+  
+        md: { max: "767px" },
+        // => @media (max-width: 767px) { ... }
+  
+        sm: { max: "639px" },
+        // => @media (max-width: 639px) { ... }
+  
+        smm: { max: "415px" },
+        // => @media (max-width: 415px) { ... }
+      },
+    },
   },
   plugins: [],
 }
